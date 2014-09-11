9b9c3a873a63fc62cc58409c81f414bb17b1ff1c
Hajinsol <jinsor5542@gmail.com>
2014-09-11 15:37:56 +0900

    8

--- sketch.js
+++ sketch.js
@@ -18,7 +18,8 @@ function setup() {
     x = 3;
     ellipse(x * 150, 100, 50, 50);
 */
- for (var x = 1; x <=3; x++)
+//for (시작상태; 반복하는 조건; 증가값)
+ for (var x = 1; x <=8; x++)
   {
     ellipse(x * 100, 100, 50, 50);
   }
